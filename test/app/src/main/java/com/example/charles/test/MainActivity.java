97db28625782b62d23689cc0456b454dd5728f10
Charles Dufour <charles.dufour@epfl.ch>
2018-11-24 22:18:11 +0100

    sound_check

---
+++ test/app/src/main/java/com/example/charles/test/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.charles.test;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player1;
+    MediaPlayer player2;
+    MediaPlayer player3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void play1(View v) {
+        if (player1 == null) {
+            player1 = MediaPlayer.create(this, R.raw.song);
+            player1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+
+        player1.start();
+    }
+
+    public void play2(View v) {
+        if (player2 == null) {
+            player2 = MediaPlayer.create(this, R.raw.high_freak);
+            player2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+
+        player2.start();
+    }
+
+    public void play3(View v) {
+        if (player3 == null) {
+            player3 = MediaPlayer.create(this, R.raw.mid_freak);
+            player3.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+
+        player3.start();
+    }
+
+    public void start_music(View v){
+
+        play1(v);
+        play2(v);
+        play3(v);
+    }
+
+    public void pause(View v) {
+        if (player1 != null) {
+            player1.pause();
+        }
+        if (player2 != null) {
+            player2.pause();
+        }
+        if (player3 != null){
+            player3.pause();
+        }
+    }
+
+    public void stop(View v) {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player1 != null) {
+            player1.release();
+            player1 = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+        if (player2 != null) {
+            player2.release();
+            player2 = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+        if (player3 != null) {
+            player3.release();
+            player3 = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+}
